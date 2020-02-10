6ec3ac4ae13be459b085b2188df38ab347e7ad5e
lda518 <57947137+lda518@users.noreply.github.com>
2020-02-10 20:36:40 +0000

    Getting the attack functionality of the patrol down

--- core/src/com/septagon/entites/Patrol.java
+++ core/src/com/septagon/entites/Patrol.java
@@ -15,31 +15,36 @@ public class Patrol extends Vehicle  {
 
     public Patrol(int col, int row, Texture texture, int health, int damage, int range, int speed, char direction, ArrayList<Tile> path){
         super(col, row, texture, health, damage, range, speed);
-        this.direction = direction;
         this.path = path;
     }
 
-
-    //Function to attack Fire Engine, same as fortress. Refactor into same class at some point?
-    public void DamageEngineIfInRange(Engine fireEngine) {
-
+    //Checks whether a fireEngine is within its range. Return true if it is, return false if it isnt
+    private boolean inRange(Engine fireEngine) {
         int engineCol = fireEngine.getCol();
         int engineRow = fireEngine.getRow();
 
         int xDisplacement = Math.min(Math.abs(this.getCol() - engineCol), Math.abs((this.getCol() + (this.width / 32)) - engineCol));
         int yDisplacement = Math.min(Math.abs(this.getRow() - engineRow), Math.abs((this.getRow() + (this.height / 32)) - engineRow));
 
+        if (Math.sqrt((xDisplacement * xDisplacement) + (yDisplacement * yDisplacement)) <= this.range) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //TODO implement shooting mechanism
+    private void shoot(Engine fireEngine){
+        int numBullets = 25;
+
         int engineX = fireEngine.getX();
         int engineY = fireEngine.getY();
 
         int centreX = this.x + (this.width/2);
-        int centreY = this.y + (this.height/2);;
-
-        //define the number of projectiles are drawn when an entity is shot
-        int numBullets = 25;
+        int centreY = this.y + (this.height/2);
 
         //Actual functionality
-        if(Math.sqrt( (xDisplacement * xDisplacement) + (yDisplacement * yDisplacement) ) <= this.range){
+        if(inRange(Engine fireEngine)){
             fireEngine.takeDamage(this.damage);
 
             for (int i = 0; i< numBullets; i++){
@@ -47,4 +52,9 @@ public class Patrol extends Vehicle  {
             }
         }
     }
-}
+
+    //TODO implement movement mechanism
+    private void move(){
+        }
+    }
+
